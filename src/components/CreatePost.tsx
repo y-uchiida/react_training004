3f00e5d423e4d3e73807fc4027f3d96f542f9fa7
yugo uchiida <y.uchiida@nirneege.com>
2022-09-12 03:15:13 +0900

    implement store posts data in Firestore collection

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import { addDoc, collection, } from 'firebase/firestore';
+import React, { useState } from 'react'
 import './CreatePost.css'
+import { db, auth } from '../firebase'
+import { useNavigate } from 'react-router-dom';
 
 export const CreatePost = () => {
+
+	/* ナビゲーションの変更(リダイレクト)を行うためのオブジェクトを取得 */
+	const navigate = useNavigate();
+
+	const [title, setTitle] = useState('');
+	const [postContent, setPostContent] = useState('');
+
+	/**
+	 * fireStore に要してあるposts コレクションに、記事データを投稿する
+	 */
+	const createPost = async () => {
+		await addDoc(collection(db, 'posts'), {
+			title,
+			postContent,
+			author: {
+				id: auth.currentUser?.uid,
+				userName: auth.currentUser?.displayName,
+			}
+		}).then(() => {
+			navigate('/');
+		});
+	}
+
 	return (
 		<div className='createPostPage'>
 			<div className="postContainer">
 				<h1>記事を投稿する</h1>
 				<div className="inputPost">
 					<div>タイトル</div>
-					<input type="text" placeholder='タイトルを記入' />
+					<input type="text" placeholder='タイトルを記入' onChange={(e) => setTitle(e.target.value)} />
 				</div>
 				<div className="inputPost">
 					<div>投稿</div>
-					<textarea placeholder='内容を記入'></textarea>
+					<textarea placeholder='内容を記入' onChange={(e) => setPostContent(e.target.value)}></textarea>
 				</div>
-				<button className='postButton'>投稿する</button>
+				<button className='postButton' onClick={createPost}>投稿する</button>
 			</div>
-		</div >
+		</div>
 	)
 }
