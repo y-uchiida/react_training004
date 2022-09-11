ad93357486c49d1ca35a9bec4c0435428759e863
yugo uchiida <y.uchiida@nirneege.com>
2022-09-12 03:13:52 +0900

    add post form and style it

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './CreatePost.css'
 
 export const CreatePost = () => {
 	return (
-		<div>CreatePost</div>
+		<div className='createPostPage'>
+			<div className="postContainer">
+				<h1>記事を投稿する</h1>
+				<div className="inputPost">
+					<div>タイトル</div>
+					<input type="text" placeholder='タイトルを記入' />
+				</div>
+				<div className="inputPost">
+					<div>投稿</div>
+					<textarea placeholder='内容を記入'></textarea>
+				</div>
+				<button className='postButton'>投稿する</button>
+			</div>
+		</div >
 	)
 }
